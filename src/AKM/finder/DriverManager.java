a9c72b4af8aaf253258c6710ca1370a21cd6737b
keshav1508 <kkdubey@mitaoe.ac.in>
2022-04-25 02:05:52 +0530

    initial commit

---
+++ src/AKM/finder/DriverManager.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AKM.finder;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author hp1
+ */
+class DriverManager {
+
+    static Connection getConnection(String jdbcoraclethinlocalhost1521xe, String system, String keshav15) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
